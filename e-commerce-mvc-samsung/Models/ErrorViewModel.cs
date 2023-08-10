9f2e016d3b299a3b35435ed0c4e308c3b5e865c2
kollipara <sonakollipara@gmail.com>
2023-08-10 21:24:12 +0530

    e-commerce first commit

---
+++ e-commerce-mvc-samsung/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace e_commerce_mvc_samsung.Models
+{
+	public class ErrorViewModel
+	{
+		public string RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
